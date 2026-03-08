ae95b0dbc17fc06ba1a5672dda2d02163b41b8a7
heriksevak <msevak4@gmail.com>
2026-03-08 16:03:51 +0530

    fix railway port binding

--- ChatRelay.API/Program.cs
+++ ChatRelay.API/Program.cs
@@ -37,13 +37,30 @@
 
 //var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 //app.Run($"http://0.0.0.0:{port}");
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
+
+//var app = builder.Build();
+
+//app.MapGet("/", () => "ChatRelay API Running");
+
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+//app.Run($"http://0.0.0.0:{port}");
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(int.Parse(port));
+});
 
 var app = builder.Build();
 
 app.MapGet("/", () => "ChatRelay API Running");
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Run($"http://0.0.0.0:{port}");
+app.Run();
