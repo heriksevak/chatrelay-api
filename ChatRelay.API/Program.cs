b523d8e9445579bd8d9def29f937e712669030ee
heriksevak <msevak4@gmail.com>
2026-03-08 15:37:26 +0530

    fix railway port bindings

--- ChatRelay.API/Program.cs
+++ ChatRelay.API/Program.cs
@@ -1,39 +1,49 @@
-using ChatRelay.API.Data;
-using Microsoft.EntityFrameworkCore;
+//using ChatRelay.API.Data;
+//using Microsoft.EntityFrameworkCore;
 
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//builder.Services.AddHostedService<MessageWorker>();
+//builder.Services.AddHttpClient<WhatsAppService>();
+
+////var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+////if (!string.IsNullOrEmpty(connectionString))
+////{
+////    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+////        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+////}
+////else
+////{
+////    Console.WriteLine("DefaultConnection not found.");
+////}
+
+//var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseAuthorization();
+//app.UseMiddleware<ApiKeyMiddleware>();
+
+//app.MapControllers();
+
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+//app.Run($"http://0.0.0.0:{port}");
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddHostedService<MessageWorker>();
-builder.Services.AddHttpClient<WhatsAppService>();
-
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-//if (!string.IsNullOrEmpty(connectionString))
-//{
-//    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-//}
-//else
-//{
-//    Console.WriteLine("DefaultConnection not found.");
-//}
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-app.UseMiddleware<ApiKeyMiddleware>();
-
-app.MapControllers();
+app.MapGet("/", () => "ChatRelay API Running");
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Run($"http://0.0.0.0:{port}");
