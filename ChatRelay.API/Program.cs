7ecc88dde3a66f8f2827ef254a09bde429b75f06
heriksevak <msevak4@gmail.com>
2026-03-08 15:33:08 +0530

    temporarily disable mysql

--- ChatRelay.API/Program.cs
+++ ChatRelay.API/Program.cs
@@ -3,34 +3,37 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddHostedService<MessageWorker>();
 builder.Services.AddHttpClient<WhatsAppService>();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
-    ));
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//if (!string.IsNullOrEmpty(connectionString))
+//{
+//    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+//}
+//else
+//{
+//    Console.WriteLine("DefaultConnection not found.");
+//}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 app.UseMiddleware<ApiKeyMiddleware>();
+
 app.MapControllers();
-app.UseHttpsRedirection();
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Run($"http://0.0.0.0:{port}");
